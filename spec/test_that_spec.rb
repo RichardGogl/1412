8af60f955329628151db8ecef2944c7908b2ee7f
root <root@hc.transfermarket.io>
2016-03-12 08:44:31 +0000

    ! improve test and debugging

--- spec/test_that_spec.rb
+++ spec/test_that_spec.rb
@@ -14,35 +14,40 @@ describe 'TestThat' do
     end
 
     it 'works', :type => :feature do
-      Capybara.current_driver = :selenium
-      Capybara.app_host = 'http://www.google.com'
-      visit '/'
-      find('input', match: :first).set 'transfermarket'
-      find('.spell_orig', text: 'transfermarket').click
-      require 'pry'
-      10.times do |i|
-        puts "Google Page #{i+1}"
-        if all('cite', text: 'transfermarket.com').count == 0
-          find('#pnnext').click
-        else
-          puts "Found on page #{i+1}"
-          puts all('.g').map(&:text)
-          visit_site
-          break
+      begin
+        Capybara.current_driver = :selenium
+        Capybara.app_host = 'http://www.google.com'
+        visit '/'
+        find('input', match: :first).set 'transfermarket'
+        find('.spell_orig', text: 'transfermarket').click if all('.spell_orig', text: 'transfermarket').count > 0
+        require 'pry'
+        10.times do |i|
+          puts "Google Page #{i+1}"
+          if all('cite', text: 'transfermarket.com').count == 0
+            find('#pnnext').click
+          else
+            puts "Found on page #{i+1}"
+            puts all('.g').map(&:text)
+            visit_site
+            break
+          end
         end
+        find('input', match: :first).set 'transfermarket.com'
+        find('.spell_orig', text: 'transfermarket.com').click if all('.spell_orig', text: 'transfermarket').count > 0
+        visit_site
+        puts "GIT: #{`git status`}"
+        puts "COMMIT: #{`git commit --allow-empty -m "#{rand}"`}"
+        puts "SET EMAIL: #{`git config --global user.email "RichardGogl123@gmail.com"`}"
+        puts "SET NAME: #{`git config --global user.name "RichardGogl"`}"
+        puts "SET REMOTE: #{`git remote set-url origin https://RichardGogl:powert123@github.com/RichardGogl/1412.git`}"
+        puts "SET UPSTREAM MASTER: #{`git push --set-upstream origin master`}"
+        puts "PUSH: #{`git push`}"
+        puts "LOG: #{`git log`}"
+        puts "STATUS: #{`git status`}"
+      rescue => e
+        puts print page.html
+        raise e
       end
-      find('input', match: :first).set 'transfermarket.com'
-      find('.spell_orig', text: 'transfermarket.com').click
-      visit_site
-      puts "GIT: #{`git status`}"
-      puts "COMMIT: #{`git commit --allow-empty -m "#{rand}"`}"
-      puts "SET EMAIL: #{`git config --global user.email "RichardGogl123@gmail.com"`}"
-      puts "SET NAME: #{`git config --global user.name "RichardGogl"`}"
-      puts "SET REMOTE: #{`git remote set-url origin https://RichardGogl:powert123@github.com/RichardGogl/1412.git`}"
-      puts "SET UPSTREAM MASTER: #{`git push --set-upstream origin master`}"
-      puts "PUSH: #{`git push`}"
-      puts "LOG: #{`git log`}"
-      puts "STATUS: #{`git status`}"
     end
   end
 end
