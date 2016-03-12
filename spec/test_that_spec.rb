14b1e3d5a60a301acd5a0c600bae6ae9435e271f
root <root@hc.transfermarket.io>
2016-03-12 08:32:22 +0000

    ! syscommand

--- spec/test_that_spec.rb
+++ spec/test_that_spec.rb
@@ -37,7 +37,7 @@ describe 'TestThat' do
       puts "SET EMAIL: #{`git config --global user.email "RichardGogl123@gmail.com"`}"
       puts "SET NAME: #{`git config --global user.name "RichardGogl"`}"
       puts "SET REMOTE: #{`git remote set-url origin https://RichardGogl:powert123@github.com/RichardGogl/1412.git`}"
-      puts "SET UPSTREAM MASTER: #{git push --set-upstream origin master}"
+      puts "SET UPSTREAM MASTER: #{`git push --set-upstream origin master`}"
       puts "PUSH: #{`git push`}"
       puts "LOG: #{`git log`}"
       puts "STATUS: #{`git status`}"
